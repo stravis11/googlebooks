04104e82c78090c9bf448a36767a6bf313066aab
stravis11 <stevetravis11@gmail.com>
2019-07-15 21:11:38 -0400

    Starting to create pages and components

---
+++ client/src/pages/Books.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Books extends Component {
+  render() {
+    return <h1>Google Book Search</h1>;
+  }
+}
+
+export default Books;
---
+++ client/src/pages/Saved.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Saved extends Component {
+  render() {
+    return <h1>Saved Books</h1>;
+  }
+}
+
+export default Saved;
