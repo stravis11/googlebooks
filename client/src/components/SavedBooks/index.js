d68efe346627c1d958b06036f246e2e6fb3de836
stravis11 <stevetravis11@gmail.com>
2019-07-18 15:59:51 -0400

    created form, jumbotron, list, and savebooks components

---
+++ client/src/components/SavedBooks/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Thumbnail from "../Thumbnail";
+import { Container, Row, Col } from "../Grid";
+
+export function SavedBook({ children }) {
+  return <ul className="list-group">{children}</ul>;
+}
+
+export function SavedBookItem({
+  thumbnail = "https://placehold.it/300x300",
+  title,
+  authors,
+  description,
+  href,
+  id,
+  deleteBook
+}) {
+  return (
+    <li className="list-group-item">
+      <Container>
+        <Row>
+          <Col size="xs-4 sm-2">
+            <Thumbnail src={thumbnail} />
+          </Col>
+          <Col size="xs-8 sm-9">
+            <a
+              href={href}
+              className="btn btn-info link"
+              target="_blank"
+              rel="noopener noreferrer"
+              role="button"
+            >
+              Read Book
+            </a>
+            <button
+              type="button"
+              onClick={() => deleteBook(id)}
+              className="btn btn-danger"
+            >
+              Delete Book
+            </button>
+            <h3>{title}</h3>
+            <h3>{authors}</h3>
+            <p>Synopsis: {description}</p>
+          </Col>
+        </Row>
+      </Container>
+    </li>
+  );
+}
